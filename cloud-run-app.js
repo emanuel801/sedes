b81b529c3a8932f284249807c442b35958af8e8d
emanuelxd <emanuel.801.r@gmail.com>
2024-02-20 21:47:03 -0600

    makefile

--- cloud-run-app.js
+++ cloud-run-app.js
@@ -1,4 +1,4 @@
-/*
+
 const express = require('express');
 //const redis = require('redis');
 const app = express();
@@ -216,49 +216,4 @@ app.get('/sedes', (req, res) => {
 // Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
-});
-
-*/
-
-
-/**
- * Cloud Run application that generates and delivers dynamiocally generated content.
- */
-
-const express = require('express');
-const cors = require('cors')
-const redis = require("redis");
-
-const app = express();
-
-const client = redis.createClient({
-  "host": "172.17.55.3"
-});
-client.on("error", function(error) {
-  console.error(error);
-});
-
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.set('Cache-Control', 'no-store');
-  client.set("key", "value!", redis.print);
-  client.get("key", (err, reply) => {
-    res.send(`
-    <html>
-      <head>
-      </head>
-      <body>
-        <p>Connecting to Redis at: ${process.env.REDIS_IP}</p>
-        <p>Value of key just read: ${reply}</p>
-      </body>
-    </html>
-    `);
-    });
-  });
-
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
 });
